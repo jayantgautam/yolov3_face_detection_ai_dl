5cd3d7b633adf5035893f0ab6a25a2fd3afbb78f
Jayant Gautam <jayantgautam1208@gmail.com>
2026-09-15 23:31:46 +0200

    Refactor k-means anchor calculation in Calc_Anchors_kMeans.py

--- Calc_Anchors_kMeans.py
+++ Calc_Anchors_kMeans.py
@@ -4,7 +4,7 @@ def iou_wh(box, clusters):
     
     """
     Calculate the IoU between a box and cluster anchors.
-    box: numpy array of shape (2,) [w, h]
+    box: numpy array of shape (2,) [w, h ]
     clusters: numpy array of shape (k, 2)
     """
     
